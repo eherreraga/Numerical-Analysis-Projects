506a01dfe4da61cb332231d3159e4e10558f6532
Eddie <eherrerag.eh@gmail.com>
2018-11-11 13:17:39 -0700

    Changed the program because I misunderstood what was being ask for in the homework

--- Error in Solving Linear Systems/ResidualCalculation.m
+++ Error in Solving Linear Systems/ResidualCalculation.m
@@ -1,8 +1,7 @@
 function ResidualCalculation(A,b)
 %(i)Create or read A and b(ii) Solve for Ax = b using GEpivot, calling
-%computed splution x.(iii) Compute residual r = b - Ax, using the procedure
-%described following(6.78). (iv) Solve for the error in Ae = r, calling
-%the solution e.(v) Print e and x+e. Also print ||e||/||x||.
+%computed solution x.(iii) Compute residual r = b - Ax, using the procedure
+%described following(6.78).
 
 %Problem 1c) H5x = [1, .06, 0.4, 0.3, 0.3]^T, where is H5 defined in (6.92)
 
@@ -12,19 +11,7 @@ function ResidualCalculation(A,b)
 %Compute the residual
 r = b - A * x;
 
-%Solve for the error in Ae = r, calling the solution r
-[e, ~, ~] = GEpivot(A, r);
+fprintf('x:%5.f\tr:%.5f', x, r);
 
-%Print e and x+e
-t = e + x;
-fprintf('e: %.5f\nx+e:%.5f', e, t);
 
-for i = 1: 1: length(x)
-    e0 = e(i);
-    x0 = x(i);
-    
-    s = e0 + x0;
-    
-    fprintf('%15.5e\t%15.5f%15.5e\n', e0, s, asb(e0, x0));
-end
 
